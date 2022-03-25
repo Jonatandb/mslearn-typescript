49837d679b595d70af3db07c65a2e1655e288dae
Jonatandb <jonatandb@gmail.com>
2022-03-25 20:14:15 -0300

    Module 06 solved

--- module-06/m06-start/build/module06.js
+++ module-06/m06-start/build/module06.js
@@ -1,1 +1,47 @@
-// Empty
+"use strict";
+/*  Module 6: DGenerics in TypeScript
+    Lab Start */
+/*  DataStore is a utility function that can store up to 10 string values in an array.
+    Rewrite the DataStore class so the array can store items of any type.
+
+    TODO: Add and apply a type variable. */
+class DataStore {
+    constructor() {
+        this._data = new Array(10);
+    }
+    AddOrUpdate(index, item) {
+        if (index >= 0 && index < 10) {
+            this._data[index] = item;
+        }
+        else {
+            console.log('Index is greater than 10');
+        }
+    }
+    GetData(index) {
+        if (index >= 0 && index < 10) {
+            return this._data[index];
+        }
+        else {
+            return;
+        }
+    }
+}
+let cities = new DataStore();
+cities.AddOrUpdate(0, "Mumbai");
+cities.AddOrUpdate(1, "Chicago");
+cities.AddOrUpdate(11, "London"); // item not added
+console.log(cities.GetData(1)); // returns 'Chicago'
+console.log(cities.GetData(12)); // returns 'undefined'
+// TODO Test items as numbers.
+let empIDs = new DataStore();
+empIDs.AddOrUpdate(0, 50);
+empIDs.AddOrUpdate(1, 65);
+empIDs.AddOrUpdate(2, 89);
+console.log(empIDs.GetData(0)); // returns 50
+let pets = new DataStore();
+pets.AddOrUpdate(0, { name: 'a', breed: 'wsd', age: 10 });
+pets.AddOrUpdate(1, { name: 'b', breed: 'ewq', age: 12 });
+pets.AddOrUpdate(2, { name: 'c', breed: 'asd', age: 3 });
+console.log(pets.GetData(1));
+console.log(pets.GetData(0));
+console.log(pets.GetData(2));
--- module-06/m06-start/module06.ts
+++ module-06/m06-start/module06.ts
@@ -5,15 +5,15 @@
     Rewrite the DataStore class so the array can store items of any type.
 
     TODO: Add and apply a type variable. */
-class DataStore {
+class DataStore<T> {
 
-    private _data = new Array(10);
+    private _data: Array<T> = new Array(10);
     
-    AddOrUpdate(index: number, item: string) {
+    AddOrUpdate(index: number, item: T) {
         if(index >=0 && index <10) {
             this._data[index] = item;
         } else {
-            alert('Index is greater than 10')
+            console.log('Index is greater than 10')
         }
     }
     GetData(index: number) {
@@ -35,6 +35,25 @@ console.log(cities.GetData(1));         // returns 'Chicago'
 console.log(cities.GetData(12));        // returns 'undefined'
 
 // TODO Test items as numbers.
+let empIDs = new DataStore<number>();
+empIDs.AddOrUpdate(0, 50);
+empIDs.AddOrUpdate(1, 65);
+empIDs.AddOrUpdate(2, 89);                  
+console.log(empIDs.GetData(0));         // returns 50
 
 
 // TODO Test items as objects.
+type Pets = {
+    name: string,
+    breed: string,
+    age: number
+}
+
+let pets = new DataStore<Pets>()
+pets.AddOrUpdate(0, {name: 'a', breed:'wsd', age: 10})
+pets.AddOrUpdate(1, {name: 'b', breed:'ewq', age: 12})
+pets.AddOrUpdate(2, {name: 'c', breed:'asd', age: 3})
+
+console.log(pets.GetData(1));
+console.log(pets.GetData(0));
+console.log(pets.GetData(2));
